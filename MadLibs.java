5fef4300b0af5eab55ec94fad8444eafb253b341
nghodsi <navid@ghodsi.us>
2020-09-02 02:24:54 -0700

    Update MadLibs.java

--- MadLibs.java
+++ MadLibs.java
@@ -20,36 +20,33 @@ public class MadLibs {
 		//3. Read an adjectives.txt file and store its list of adjectives into an arraylist.
 
 		//4. Read an adverbs.txt file and store its list of adverbs into an arraylist.
-
-		//5. Prompt user for the name of a file containing a MadLibs story.
-		// Read the story from that file and write it to System.out, but replacing each instance of
-		// <<NOUN>>, <<VERB>>, etc. with a randomly chosen word from the corresponding list.
 		Scanner kb = new Scanner(System.in);
-		System.out.print ("Gimme a MadLibs story file");
+		System.out.print ("Gimme a list of adverbs");
 		String fileName = kb.next();
-		File story = new File (fileName);
-		Scanner reader = new Scanner(story);
+		File words = new File (fileName);
+		Scanner reader = new Scanner(words);
 		while (reader.hasNextLine()) {
 			String line = reader.nextLine();
 			String word = "";
 			int num = 0;
-			while (line.substring(num,num+1)!= " ") {
-				word += line.substring(num,num+1);
+			while (line.substring(num, num+1) != null) {
+				if (!line.substring(num,num+1).equals(" ")) {
+					word += line.substring(num,num+1);
+				}
+				else {
+					adv.add(word);
+					word = "";
+				}
+				num++;
 			}
-			if (word == "<<NOUN>>") {
-				word = nouns.get(Math.random()*nouns.size());
-				System.out.print(word + " ");
-			}
-			else if (word == "<<VERB>>") {
-				word = verbs.get(Math.random()*verbs.size());
-				System.out.print(word + " ");
-			}
-			else (!word.equals("<<NOUN>>") && !word.equals("<<VERB>>")) {
-				System.out.print(word + " ");
-			}
-			
-
 		}
+			
+		
+
+		//5. Prompt user for the name of a file containing a MadLibs story.
+		// Read the story from that file and write it to System.out, but replacing each instance of
+		// <<NOUN>>, <<VERB>>, etc. with a randomly chosen word from the corresponding list.
+		
 		
 
 	}
