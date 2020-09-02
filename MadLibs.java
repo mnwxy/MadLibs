1a3f07d1ecc88b908bb647afbd7ff6148777fd0c
mnwxy <william.k.tao@gmail.com>
2020-09-02 09:10:54 -0700

    Reverting the master branch to the last usable state
    Improper merge conflict handling broke the master branch. This commit should fix that.

--- MadLibs.java
+++ MadLibs.java
@@ -1,14 +1,10 @@
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.*;
-
 
 public class MadLibs {
 
 	public static void main(String[] args) {
 		//0. Give variable names for each of the arraylists below and create Madlibs stories.
-		
+
 		ArrayList<String> nouns = new ArrayList<String>();
 		ArrayList<String> verbs = new ArrayList<String>();
 		ArrayList<String> adj = new ArrayList<String>();
@@ -20,23 +16,10 @@ public class MadLibs {
 		//3. Read an adjectives.txt file and store its list of adjectives into an arraylist.
 
 		//4. Read an adverbs.txt file and store its list of adverbs into an arraylist.
-		Scanner kb = new Scanner(System.in);
-		System.out.print ("Gimme a list of adverbs");
-		String fileName = kb.next();
-		File words = new File (fileName);
-		Scanner reader = new Scanner(words);
-		while (reader.hasNextLine()) {
-			String line = reader.nextLine();
-			adv.add(line);
-		}
-			
-		
 
 		//5. Prompt user for the name of a file containing a MadLibs story.
 		// Read the story from that file and write it to System.out, but replacing each instance of
 		// <<NOUN>>, <<VERB>>, etc. with a randomly chosen word from the corresponding list.
-		
-		
 
 	}
 
